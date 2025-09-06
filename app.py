aff27ba88d95eb77f088228889838374b0bfaef6
Samorukoff <nikita.samorukov@gmail.com>
2025-09-06 10:02:02 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -3,10 +3,27 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return("""<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
+           </body>
+        </html>""")
+
+@app.route("/author")
+def author():
+
+    name = "Саморуков Никита Олегович"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+
+    return("""<!doctype html>
+        <html>
+           <body>
+               <p>Студент: """ + name + """</p>
+               <p>Группа: """ + group + """</p>
+               <p>Факультет: """ + faculty + """</p>
            </body>
         </html>""")
