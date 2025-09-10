8a77aa41956e2a79eccc85fb991daee9b316e887
Samorukoff <nikita.samorukov@gmail.com>
2025-09-10 12:44:37 +0700

    Стили

--- app.py
+++ app.py
@@ -35,13 +35,17 @@ def author():
 @app.route("/image")
 def image():
 
-    path = url_for("static", filename= "oak.jpg")
+    path = url_for("static", filename = "oak.jpg")
+    style = url_for("static", filename = "lab1.css")
 
     return'''<!doctype html>
         <html>
+           <head>
+               <link rel="stylesheet" href="''' + style + '''">
+           </head>
            <body>
                <h1>Дуб</h1>
-               <img src="''' + path + '''">
+                <img src="''' + path + '''">
            </body>
         </html>'''
 
