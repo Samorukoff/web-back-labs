387b4b0d51a5ac8d7954d5854c7e8e1b8508a6cd
Samorukoff <nikita.samorukov@gmail.com>
2025-09-20 10:57:29 +0700

    Имя студента

--- app.py
+++ app.py
@@ -351,4 +351,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Саморуков Никита'
+    return render_template('example.html', name=name)
