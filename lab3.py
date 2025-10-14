00c4bbece176be85d04f0bf72d0f147f7fd02957
Samorukoff <nikita.samorukov@gmail.com>
2025-10-14 19:45:59 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -7,7 +7,7 @@ lab3 = Blueprint('lab3', __name__)
 def lab():
     name = request.cookies.get('name')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3.html', name=name,
+    return render_template('lab3/lab3.html', name=name,
                                         name_color = name_color)
 
 
@@ -27,3 +27,13 @@ def del_cookie():
     resp.delete_cookie('age')
     resp.delete_cookie('name_color')
     return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user,
+                                              age=age,
+                                              sex=sex)
