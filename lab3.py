661b2cb2b05fc4ba5889a277dd1abf0fc3301bb1
Samorukoff <nikita.samorukov@gmail.com>
2025-10-17 23:01:47 +0700

    Аноним

--- lab3.py
+++ lab3.py
@@ -5,10 +5,12 @@ lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
 def lab():
-    name = request.cookies.get('name')
-    name_color = request.cookies.get('name_color')
+    name = request.cookies.get('name') or 'Аноним'
+    age = request.cookies.get('age') or 'Неизвестный'
+    name_color = request.cookies.get('name_color') or 'black'
     return render_template('lab3/lab3.html', name=name,
-                                        name_color = name_color)
+                                             age=age,
+                                             name_color=name_color)
 
 
 @lab3.route('/lab3/cookie')
