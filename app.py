48267b2b2cc092f0c4dfe79ccd0c59f9508b7acb
Samorukoff <nikita.samorukov@gmail.com>
2025-12-26 01:26:58 +0700

    Самостоятельные задания

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ login_manager.init_app(app)
 def load_user(login_id):
     return users.query.get(int(login_id))
 
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'Секретно-секретный-секрет')
+app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'Секретно-секретный-секрет').encode('utf-8')
 app.config['DB_TYPE']=os.getenv('DB_TYPE', 'postgres')
 
 if app.config['DB_TYPE'] == 'postgres':
